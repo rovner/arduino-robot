fd67fd7605423d8424168a0d10f90799b1267862
rovner <rovner@yandex-team.ru>
2016-03-05 22:13:39 +0300

    more ir codes

--- src/IRCodes.h
+++ src/IRCodes.h
@@ -6,5 +6,24 @@
 #define ARDUINO_IRCODES_H
 
 #define POWER 0xFFA25D
+#define MENU 0xFFE21D
+#define TEST 0xFF22DD
+#define PLUS 0xFF02FD
+#define BACK 0xFFC23D
+#define PREVIOUS 0xFFE01F
+#define PLAY 0xFFA857
+#define NEXT 0xFF906F
+#define MINUS 0xFF9867
+#define C 0xFFB04F
+#define _0 0xFF6897
+#define _1 0xFF30CF
+#define _2 0xFF18E7
+#define _3 0xFF7A85
+#define _4 0xFF10EF
+#define _5 0xFF38C7
+#define _6 0xFF5AA5
+#define _7 0xFF42BD
+#define _8 0xFF4AB5
+#define _9 0xFF52AD
 
 #endif //ARDUINO_IRCODES_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,41 @@
-#include <sys/cdefs.h>
 #include <Arduino.h>
 #include <IRremote.h>
+#include "IRCodes.h"
 
 #define LED_PIN 12
 
-IRrecv iRrecv(11, 13);
+IRrecv iRrecv(11);
 
 decode_results results;
 
-void __unused setup() {
+void setup() {
     pinMode(LED_PIN, OUTPUT);
     Serial.begin(9600);
     Serial.println("start");
     iRrecv.enableIRIn();
 }
 
-void __unused loop() {
-    digitalWrite(LED_PIN, HIGH);
+void loop() {
     if (iRrecv.decode(&results)) {
+        if (results.value == POWER) {
+            digitalWrite(LED_PIN, HIGH);
+            delay(50);
+            digitalWrite(LED_PIN, LOW);
+            delay(50);
+            digitalWrite(LED_PIN, HIGH);
+            delay(50);
+            digitalWrite(LED_PIN, LOW);
+            delay(50);
+            digitalWrite(LED_PIN, HIGH);
+            delay(50);
+            digitalWrite(LED_PIN, LOW);
+            delay(50);
+        } else {
+            digitalWrite(LED_PIN, HIGH);
+            delay(100);
+            digitalWrite(LED_PIN, LOW);
+        }
         Serial.println(results.value, HEX);
-        digitalWrite(LED_PIN, LOW);
-        delay(200);
         iRrecv.resume();
     }
 }
