e58f8cbf703744738f03b84dfef4befd549b4a5b
rovner <rovner@yandex-team.ru>
2016-03-05 22:11:58 +0300

    delete libs src, install them as dependency instead add IRCodes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ###My Arduino С project :)
+
---
+++ src/IRCodes.h
@@ -0,0 +1,10 @@
+//
+// Created by Vyacheslav Rovnyakov on 05/03/16.
+//
+
+#ifndef ARDUINO_IRCODES_H
+#define ARDUINO_IRCODES_H
+
+#define POWER 0xFFA25D
+
+#endif //ARDUINO_IRCODES_H
